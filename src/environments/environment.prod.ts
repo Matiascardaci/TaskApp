4ac8ac64767b77314a8239cb1754f4b778fb0559
Enrique Roulier <roulierej23@gmail.com>
2023-09-22 15:06:36 -0300

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCNeQUVsGBICflbJTFW0pOjjFikZdbYHiM",
+    authDomain: "taskapp-84d6b.firebaseapp.com",
+    projectId: "taskapp-84d6b",
+    storageBucket: "taskapp-84d6b.appspot.com",
+    messagingSenderId: "707862466899",
+    appId: "1:707862466899:web:55ff275f55ec79c55251a6"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCNeQUVsGBICflbJTFW0pOjjFikZdbYHiM",
+    authDomain: "taskapp-84d6b.firebaseapp.com",
+    projectId: "taskapp-84d6b",
+    storageBucket: "taskapp-84d6b.appspot.com",
+    messagingSenderId: "707862466899",
+    appId: "1:707862466899:web:55ff275f55ec79c55251a6"
+  }
 };
 
 /*
